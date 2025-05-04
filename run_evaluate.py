e266fd2b17764e4256e631a4ea10327396d0a505
king17pvp <tuanhai1304@gmail.com>
2025-05-04 15:09:16 +0700

    Added csv write

--- run_evaluate.py
+++ run_evaluate.py
@@ -1,5 +1,6 @@
 import argparse 
 import os
+import csv
 import logging
 import pathlib
 import numpy as np
@@ -51,53 +52,55 @@ def evaluate(
     reranker_model_name = "checkpoint-pubmedbert-10000", 
     top_k = 50
 ):
+    results_dir = os.path.join(".", "results")
+    os.makedirs(results_dir, exist_ok=True)
     datasets = ['trec-covid', 'nfcorpus', 'scifact', 'scidocs']
     dense_model_path = f"./ckpts/biencoder-checkpoints/checkpoint-{dense_model_name}"
     reranker_model_path = f"./ckpts/crossencoder-checkpoints/checkpoint-{reranker_model_name}"
+    csv_file_path = os.path.join(results_dir, f"{dense_model_name}-retriever-{reranker_model_name}-reranker.csv")
     dense_model = models.SentenceBERT(dense_model_path)
     reranker = Rerank(CrossEncoder(reranker_model_path), batch_size=32)
-    for dataset in datasets :
-        url = f"https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{dataset}.zip"
-        out_dir = os.path.join(".", "datasets")
-        data_path = util.download_and_unzip(url, out_dir)
-        corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
+    with open(csv_file_path, mode="w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Dataset", f"NDCG@{top_k}", f"MAP@{top_k}", f"Recall@{top_k}", f"Precision@{top_k}", f"MRR@{top_k}"])
 
-        bm25_model = BM25(index_name=dataset, hostname=hostname, initialize=initialize)
-        dense_retriever = EvaluateRetrieval(DRES(dense_model), score_function= score_function, k_values=[top_k])
-        dense_results = dense_retriever.retrieve(corpus, queries)
-        bm25_results = bm25_model.search(corpus, queries, top_k, score_function)
+        for dataset in datasets :
+            url = f"https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{dataset}.zip"
+            out_dir = os.path.join(".", "datasets")
+            data_path = util.download_and_unzip(url, out_dir)
+            corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
 
-        rrf_results = reciprocal_rank_fusion([bm25_results, dense_results])
-        new_corpus = {}
-        for query_id in rrf_results:
-            if len(rrf_results[query_id]) > top_k:
-                for doc_id, _ in sorted(rrf_results[query_id].items(), key=lambda item: item[1], reverse=True)[:top_k]:
-                    new_corpus[doc_id] = corpus[doc_id]
-            else:
-                for doc_id in rrf_results[query_id]:
-                    new_corpus[doc_id] = corpus[doc_id]
-        ce_results = reranker.rerank(new_corpus, queries, rrf_results, top_k=top_k)
-        ndcg, _map, recall, precision = EvaluateRetrieval.evaluate(qrels, ce_results, [top_k])
-        mrr = EvaluateRetrieval.evaluate_custom(qrels, ce_results, metric="mrr", k_values=[top_k])
-        
-        results_dir = os.path.join(".", "results")
-        os.makedirs(results_dir, exist_ok=True)
-        with open(os.path.join(results_dir, f"{dense_model_name}-retriever-{reranker_model_name}-reranker.txt"), "w") as fw:
-            fw.write(f"NDCG@{top_k}: {ndcg[f'NDCG@{top_k}']}\n")
-            fw.write(f"MAP@{top_k}: {_map[f'MAP@{top_k}']}\n")
-            fw.write(f"Recall@{top_k}: {recall[f'Recall@{top_k}']}\n")
-            fw.write(f"Precision@{top_k}: {precision[f'P@{top_k}']}\n")
-            fw.write(f"MRR@{top_k}: {mrr[f'MRR@{top_k}']}\n")
-        print(f"NDCG@{top_k}: {ndcg[f'NDCG@{top_k}']}")
-        print(f"MAP@{top_k}: {_map[f'MAP@{top_k}']}")
-        print(f"Recall@{top_k}: {recall[f'Recall@{top_k}']}")
-        print(f"Precision@{top_k}: {precision[f'P@{top_k}']}")
-        print(f"MRR@{top_k}: {mrr[f'MRR@{top_k}']}")
+            bm25_model = BM25(index_name=dataset, hostname=hostname, initialize=initialize)
+            dense_retriever = EvaluateRetrieval(DRES(dense_model), score_function= score_function, k_values=[top_k])
+            dense_results = dense_retriever.retrieve(corpus, queries)
+            bm25_results = bm25_model.search(corpus, queries, top_k, score_function)
 
-        with open(os.path.join(results_dir, f"{dataset}-ce-top{top_k}.txt"), "w") as f:
-            for query_id in ce_results:
-                for doc_id, score in sorted(ce_results[query_id].items(), key=lambda x: -x[1]):
-                    f.write(f"{query_id}\t{doc_id}\t{score}\n")
+            rrf_results = reciprocal_rank_fusion([bm25_results, dense_results])
+            new_corpus = {}
+            for query_id in rrf_results:
+                if len(rrf_results[query_id]) > top_k:
+                    for doc_id, _ in sorted(rrf_results[query_id].items(), key=lambda item: item[1], reverse=True)[:top_k]:
+                        new_corpus[doc_id] = corpus[doc_id]
+                else:
+                    for doc_id in rrf_results[query_id]:
+                        new_corpus[doc_id] = corpus[doc_id]
+            ce_results = reranker.rerank(new_corpus, queries, rrf_results, top_k=top_k)
+            ndcg, _map, recall, precision = EvaluateRetrieval.evaluate(qrels, ce_results, [top_k])
+            mrr = EvaluateRetrieval.evaluate_custom(qrels, ce_results, metric="mrr", k_values=[top_k])
+            
+            writer.writerow([
+                dataset,
+                ndcg[f"NDCG@{top_k}"],
+                _map[f"MAP@{top_k}"],
+                recall[f"Recall@{top_k}"],
+                precision[f"P@{top_k}"],
+                mrr[f"MRR@{top_k}"]
+            ])
+            print(f"NDCG@{top_k}: {ndcg[f'NDCG@{top_k}']}")
+            print(f"MAP@{top_k}: {_map[f'MAP@{top_k}']}")
+            print(f"Recall@{top_k}: {recall[f'Recall@{top_k}']}")
+            print(f"Precision@{top_k}: {precision[f'P@{top_k}']}")
+            print(f"MRR@{top_k}: {mrr[f'MRR@{top_k}']}")
 
 if __name__ == "__main__":
     args = parse_args()
